207ac7d29e1febc7618dc8fa0ff90a47d4684428
rakhatri <rahulkhatri46@gmail.com>
2025-01-31 01:01:12 -0500

    100 Days Python Course Day 6

---
+++ Course/Day6-Functions&WhileLoops.py
@@ -0,0 +1,101 @@
+# Reeborg World Hurdle 1 Prog
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+# for i in range(6):
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+
+# Reeborg World Hurdle 2 Prog
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+#
+# while not at_goal():
+#     jump()
+
+# Reeborg World Hurdle 3 Prog
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+#
+# def jump():
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+#
+#
+# while not at_goal():
+#     if front_is_clear():
+#         move()
+#     else:
+#         jump()
+
+# Reeborg World Hurdle 4 Prog
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+#
+# def jump():
+#     turn_left()
+#     while wall_on_right():
+#         move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     while not wall_in_front():
+#         move()
+#     turn_left()
+#
+#
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     else:
+#         move()
+
+# Reeborg World Maze Prog
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+# while front_is_clear():
+#     move()
+# turn_left()
+#
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
